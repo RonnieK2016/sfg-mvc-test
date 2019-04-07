c052ba64e90f9680af6aa591b9ed0e1b4b0c7026
SergeA <ronnieo@gmail.com>
2019-04-07 17:50:11 -0400

    Swagger UI Configuration Example

--- src/main/java/com/udemy/sfgmvctest/config/SwaggerConfig.java
+++ src/main/java/com/udemy/sfgmvctest/config/SwaggerConfig.java
@@ -19,4 +19,13 @@ public class SwaggerConfig {
                 .build()
                 .pathMapping("/");
     }
+
+    //    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
 }
